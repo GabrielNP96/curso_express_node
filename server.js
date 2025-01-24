11680b0f164b41045702a04456de95544a32eabf
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-01-24 15:43:24 +0000

    add gitignore e rotas

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import http from "http";
+import { url } from "inspector";
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "Curso de Express api",
+    "/livros": "entrei na rota livros",
+    "/autores": "entrei na rota autores"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end("Curso de Node.js");
+res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
