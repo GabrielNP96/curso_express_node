0b2ddeca7ce95d929f4c352b1c887f6a6dfe2fa3
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-01-27 16:12:15 +0000

    add mongoDb

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+
+const conexao = await conectaNaDatabase();
+conexao.on("error", (erro) => {
+    console.error("Erro de conexão", erro);
+})
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso!")
+})
 
 const app = express();
 app.use(express.json());
@@ -51,3 +61,4 @@ app.delete("/livros/:id", (req, res) => {
 })
 
 export default app;
+
